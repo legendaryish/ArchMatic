ab7cf5a0b28bc5416eb89f46be089ce5d376785c
Robert Edwards <clifton.edwards@gmail.com>
2020-04-03 16:35:54 -0500

    Update 0-setupmirrors.sh

--- 0-setupmirrors.sh
+++ 0-setupmirrors.sh
@@ -1,3 +1,5 @@
+pacman -Sy
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+wget archfi.sf.net/archfi
